e0568af8305903d48c68c1389b8192234764d49a
Ruslan Korniichenko <136509723+RuslanKorni@users.noreply.github.com>
2024-02-29 13:36:36 +0100

    contacts-sevices-update

---
+++ services/contactsServices.js
@@ -0,0 +1,41 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.resolve(__dirname, "db", "contacts.json");
+
+const listContacts = async () => {
+  // Повертає масив контактів
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+const getContactById = async (id) => {
+  // Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === id);
+  return result || null;
+};
+
+const removeContact = async (id) => {
+  // Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+const addContact = async (data) => {
+  // Повертає об'єкт доданого контакту.
+  const contacts = await listContacts();
+  const newContact = {
+    id: (await import("nanoid")).nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
