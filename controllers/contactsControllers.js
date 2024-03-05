2a62c7539b057b97873a5096185fde4bdcede2ea
Ruslan Korniichenko <136509723+RuslanKorni@users.noreply.github.com>
2024-03-05 23:30:01 +0100

    finish- commit

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -6,14 +6,18 @@ import {
   updateContact as updateContactsCarr,
 } from "../services/contactsServices.js";
 
-import HttpError from '../helpers/HttpError.js';
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
 export const getAllContacts = async (_, res, next) => {
   try {
     const result = await listContacts();
     res.json(result);
   } catch (error) {
-      next(error);
+    next(error);
   }
 };
 
@@ -21,17 +25,59 @@ export const getOneContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await getContactById(id);
-      if (!result) {
-          throw HttpError(404);
+    if (!result) {
+      throw HttpError(404);
     }
     res.json(result);
   } catch (error) {
-   next(error)
+    next(error);
   }
 };
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await removeContact(id);
+    if (!result) {
+      throw HttpError(404);
+    }
 
-export const createContact = (req, res) => {};
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await addContact(req.body);
+
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const { id } = req.params;
+    const result = await updateContactsCarr(id, req.body);
+
+    if (!result) {
+      throw HttpError(404)
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
